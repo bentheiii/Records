67aa3f19a551060dcf53f1ff429001ddd0d0e997
ben avrahami <dontcallme@illcall.you>
2020-10-02 20:52:17 +0300

    inital

--- records/record.py
+++ records/record.py
@@ -205,19 +205,26 @@ class RecordBase:
         # keep track of which field are missing
         required = set(cls._required_keys)
         optional = set(cls._optional_keys)
-        try:
-            for k, v in kwargs.items():
-                required.discard(k)
-                optional.discard(k)
-                field = cls._fields.get(k)
-                if not field:
-                    raise TypeError(f'argument {k} invalid for type {cls.__qualname__}')
+        for k, v in kwargs.items():
+            required.discard(k)
+            optional.discard(k)
+            field = cls._fields.get(k)
+            if not field:
+                raise TypeError(f'argument {k} invalid for type {cls.__qualname__}')
+            try:
                 values[k] = field.filler(v)
-        except Exception:
-            # if filling failed, check if we have a parsing standing by
-            if parsing is not None:
-                return parsing
-            raise
+            except Exception as e:
+                # if filling failed, check if we have a parsing standing by
+                if parsing is not None:
+                    return parsing
+                msg = f'error filling filling {k}'
+                # try to create a parent error of the same type as the original error
+                try:
+                    parent_error = type(e)(msg)
+                except TypeError:
+                    parent_error = ValueError(msg)
+
+                raise parent_error from e
 
         if required:
             raise TypeError(f'missing required arguments: {tuple(required)}')
